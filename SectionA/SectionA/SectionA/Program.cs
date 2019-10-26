fb8a84b701be0e99a5da2b521297c5becf5e4939
Albert Bach <bach_albert@hotmail.hu>
2019-10-26 23:06:11 +0200

    SectionA done

---
+++ SectionA/SectionA/SectionA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SectionA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long numberOne = 1;
+            long numberTwo = 1000000;
+
+
+            Console.WriteLine(SumOfNumbers(numberOne,numberTwo));
+            Console.ReadLine();
+        }
+
+        public static long SumOfNumbers(long number, long anotherNumber)
+        {
+
+            long result = anotherNumber * (number + anotherNumber) / 2;
+
+            return result;
+        }
+       
+    }
+}
